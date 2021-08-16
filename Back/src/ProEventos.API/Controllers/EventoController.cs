919fa8e3e9c69e7827178e12f4980e007be21ca3
Pedro Vitor Lima Novaes <pedro.novaes@FDERIVS.com>
2021-08-16 12:25:22 -0300

    Configured environment

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento[] 
+            {
+                new Evento()
+                {
+                    EventoId = 1,
+                    DataEvento = DateTime.Now.AddDays(5).ToString(),
+                    Local = "SP",
+                    Lote = "5° Lote",
+                    QtdPessoas = 120,
+                    Tema = "Angular 11 e .Net",
+                    ImagemURL = "dvsvd",
+                }
+            };
+        }
+    }
+}
