e631c83f31929accefc07ff398e7363cf92ac85d
PedroNovaes25 <dodroxpedro28@gmail.com>
2021-09-07 09:11:16 -0300

    Layout Responsivo + Forms

--- ProEventos.Domain/PalestranteEvento.cs
+++ ProEventos.Domain/PalestranteEvento.cs
@@ -8,12 +8,10 @@ namespace ProEventos.Domain
 {
     public class PalestranteEvento
     {
-
         public int PalestranteId { get; set; }
         public Palestrante Palestrante{ get; set; }
 
         public int EventoId { get; set; }
-        public Evento Evento { get; set; }
-
+        public Evento Evento { get; set; }      
     }
 }
--- ProEventos.Persistence/DBContext/ProEventosContext.cs
+++ ProEventos.Persistence/DBContext/ProEventosContext.cs
@@ -12,9 +12,9 @@ namespace ProEventos.Persistence.DBContext
         public ProEventosContext(DbContextOptions<ProEventosContext> options) 
             : base (options){}
         public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
         public DbSet<Palestrante> Palestrantes { get; set; }
         public DbSet<PalestranteEvento> PalestranteEventos { get; set; }
-        public DbSet<Lote> Lotes { get; set; }
         public DbSet<RedeSocial> RedeSociais { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
